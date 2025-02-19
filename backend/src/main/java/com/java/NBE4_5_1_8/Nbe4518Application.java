771893f98008e0a728c796155fead35d180f0784
wnsgh <junho767@naver.com>
2025-02-19 15:45:40 +0900

    chore: 기본 세팅

---
+++ backend/src/main/java/com/java/NBE4_5_1_8/Nbe4518Application.java
@@ -0,0 +1,13 @@
+package com.java.NBE4_5_1_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe4518Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe4518Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/java/NBE4_5_1_8/Nbe4518ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.NBE4_5_1_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe4518ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
