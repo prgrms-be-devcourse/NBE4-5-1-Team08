f217720f387f45566adb4d9c74214d0c7ff9e3c4
theGreatKang98 <77500386+theGreatKang98@users.noreply.github.com>
2025-02-20 16:19:29 +0900

    fix: add login logout button condition

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import {useEffect, useState} from "react";
+import {usePathname} from "next/navigation";
 
 const Header = () => {
     const [isLogin, setIsLogin] = useState(false);
+    const pathname = usePathname();
 
     useEffect(() => {
         const userToken = localStorage.getItem("userToken");
@@ -19,18 +21,20 @@ const Header = () => {
         <header className="w-full bg-gray-900 text-white p-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-xl font-bold">Grids & Circles</h1>
-                <div>
-                    {isLogin ? (
-                        <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
-                            로그아웃
-                        </button>
-                    ) : (
-                        <button onClick={() => (window.location.href = "/login")}
-                                className="bg-blue-500 px-4 py-2 rounded">
-                            로그인
-                        </button>
-                    )}
-                </div>
+                {pathname.startsWith("/admin") && (
+                    <div>
+                        {isLogin ? (
+                            <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
+                                로그아웃
+                            </button>
+                        ) : (
+                            <button onClick={() => (window.location.href = "/login")}
+                                    className="bg-blue-500 px-4 py-2 rounded">
+                                로그인
+                            </button>
+                        )}
+                    </div>
+                )}
             </div>
         </header>
     );
