3c7be8ff12f7a1c66d8e2d25fa99798c3447a80f
JunKiBeom <dona0408@gmail.com>
2025-02-20 10:37:22 +0900

    chore: add SecurityConfig

---
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/entity/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.java.NBE4_5_1_8.global.entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.GET, "/api/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+        return http.build();
+    }
+}
