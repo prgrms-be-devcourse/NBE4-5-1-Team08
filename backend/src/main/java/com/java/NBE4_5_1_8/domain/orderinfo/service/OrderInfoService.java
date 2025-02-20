b49976decfd1cfa31167abd3bfe3c50690166ae2
JunKiBeom <dona0408@gmail.com>
2025-02-20 12:16:31 +0900

    chore: add Order Service, Controller

---
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/service/OrderInfoService.java
@@ -0,0 +1,10 @@
+package com.java.NBE4_5_1_8.domain.orderinfo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderInfoService {
+
+}
