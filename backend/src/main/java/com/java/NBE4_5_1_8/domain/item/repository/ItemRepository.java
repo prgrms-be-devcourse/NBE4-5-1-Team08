3897eefeadd1c0fe8ef76a582c4d641e219dc8a0
서세훈 <ssa0100@naver.com>
2025-02-20 16:54:00 +0900

    feat: add findByItemId

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/item/repository/ItemRepository.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/item/repository/ItemRepository.java
@@ -4,6 +4,9 @@ import com.java.NBE4_5_1_8.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findByItemId(Long itemId);
 }
