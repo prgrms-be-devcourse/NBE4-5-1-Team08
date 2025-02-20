0d4d9efb7eb8325b790697f2ac9977f9d62afd8e
JunKiBeom <dona0408@gmail.com>
2025-02-20 17:27:29 +0900

    feat: 주문 수정 기능 구현 - PUT /orders/{orderId} API 개발

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/controller/ApiV1OrderInfoController.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/controller/ApiV1OrderInfoController.java
@@ -1,12 +1,40 @@
 package com.java.NBE4_5_1_8.domain.orderinfo.controller;
 
+import com.java.NBE4_5_1_8.domain.orderinfo.entity.OrderInfo;
+import com.java.NBE4_5_1_8.domain.orderinfo.service.OrderInfoService;
+import com.java.NBE4_5_1_8.global.response.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class ApiV1OrderInfoController {
 
+    private final OrderInfoService orderInfoService;
+
+    @PutMapping("/{orderId}")
+    public RsData<OrderInfo> updateOrderInfo(
+            @PathVariable long orderId,
+            @RequestBody @Valid UpdateReqBody updateReqBody
+    ) {
+        OrderInfo orderInfo = orderInfoService.getOrderById(orderId).get();
+        orderInfoService.updateOrderInfo(
+                orderInfo,
+                updateReqBody.orderStatus(),
+                updateReqBody.memberEmail(),
+                updateReqBody.memberAddress()
+        );
+        return RsData.success(orderInfo, "주문이 성공적으로 수정되었습니다.");
+    }
+
+    // Update
+    public record UpdateReqBody(
+            @NotBlank @Length(min = 3) String orderStatus,
+            @NotBlank @Length(min = 3) String memberEmail,
+            @NotBlank @Length(min = 3) String memberAddress) {
+    }
 }
--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/service/OrderInfoService.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/service/OrderInfoService.java
@@ -1,10 +1,22 @@
 package com.java.NBE4_5_1_8.domain.orderinfo.service;
 
+import com.java.NBE4_5_1_8.domain.orderinfo.entity.OrderInfo;
+import com.java.NBE4_5_1_8.domain.orderinfo.repository.OrderInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderInfoService {
 
+    private final OrderInfoRepository orderInfoRepository;
+
+    // Update
+    @Transactional
+    public void updateOrderInfo(OrderInfo orderInfo, String orderStatus, String memberEmail, String memberAddress) {
+        orderInfo.setOrderStatus(orderStatus);
+        orderInfo.setMemberEmail(memberEmail);
+        orderInfo.setMemberAddress(memberAddress);
+    }
 }
