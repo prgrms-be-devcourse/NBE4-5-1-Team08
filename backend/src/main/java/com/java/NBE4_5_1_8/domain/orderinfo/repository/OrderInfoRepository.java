a8010c92efdf772cede42deb8e91b5f84bbaa299
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2025-02-21 15:33:07 +0900

    Revert "Feature/9 (#28)" (#32)
    This reverts commit 18c9579ecf0baf30f5f5d7857912467bb77f131e.

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/repository/OrderInfoRepository.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/repository/OrderInfoRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
-    OrderInfo findByMemberEmail(String memberEmail);
 }
