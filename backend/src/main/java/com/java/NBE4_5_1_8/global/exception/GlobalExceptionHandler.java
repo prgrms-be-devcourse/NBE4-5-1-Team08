286457483bde5c83b24756ad4544db1060c6c220
jeonghyeon-ohk <ok6737@naver.com>
2025-02-21 16:08:45 +0900

    feat: GlobalExceptionHandler에 MethodArgumentNotValidException 처리 추가
    - itemForm의 유효성 처리 과정에서 발생하는 Exception을 처리하기 위한 ExceptionHandler 생성

--- backend/src/main/java/com/java/NBE4_5_1_8/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.java.NBE4_5_1_8.global.exception;
 
 
 import com.java.NBE4_5_1_8.global.response.RsData;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,9 +14,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     public RsData<Void> ServiceExceptionHandle(ServiceException ex) {
 
-    ex.printStackTrace();
+        ex.printStackTrace();
 
         return RsData.failure(ex.getMsg());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public RsData<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        e.printStackTrace();
+
+        String message = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fe -> fe.getField() + " : " + fe.getCode() + " : " + fe.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining("\n"));
+
+        return RsData.failure(message);
     }
 
 
