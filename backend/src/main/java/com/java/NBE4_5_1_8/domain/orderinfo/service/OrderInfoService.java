d15b22c872796de8d58a146ca8073ad78cbad92c
wnsgh <junho767@naver.com>
2025-02-21 09:41:00 +0900

    feat : 주문 추가 기능 구현
    1. 주문 추가 로직 구현

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/service/OrderInfoService.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/service/OrderInfoService.java
@@ -1,10 +1,38 @@
 package com.java.NBE4_5_1_8.domain.orderinfo.service;
 
+import com.java.NBE4_5_1_8.domain.item.entity.Item;
+import com.java.NBE4_5_1_8.domain.item.repository.ItemRepository;
+import com.java.NBE4_5_1_8.domain.orderinfo.dto.OrderDto;
+import com.java.NBE4_5_1_8.domain.orderinfo.entity.OrderInfo;
+import com.java.NBE4_5_1_8.domain.orderinfo.repository.OrderInfoRepository;
+import com.java.NBE4_5_1_8.domain.orderitem.entity.OrderItem;
+import com.java.NBE4_5_1_8.domain.orderitem.repository.OrderItemRepository;
+import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderInfoService {
+    private final OrderInfoRepository orderInfoRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final ItemRepository itemRepository;
 
+    @Transactional
+    public Long addOrderInfo(OrderDto orderDto) {
+        Item item = itemRepository.findById(orderDto.getItemId())
+                .orElseThrow(EntityExistsException::new);
+
+        OrderInfo orderInfo = orderInfoRepository.findByMemberEmail(orderDto.getMemberEmail());
+        if (orderInfo == null) {
+            orderInfo = OrderInfo.createOrderInfo(orderDto.getMemberEmail());
+            orderInfoRepository.save(orderInfo);
+        }
+
+        int totalPrice = item.getPrice() * orderDto.getQuantity();
+        OrderItem orderItem = OrderItem.createOrderItem(item, orderInfo, totalPrice, orderDto.getQuantity());
+        orderItemRepository.save(orderItem);
+        return orderItem.getId();
+    }
 }
