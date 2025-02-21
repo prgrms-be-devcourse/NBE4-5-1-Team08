ee77f4ee612ce0e6595e89f29443a6b99757982b
wnsgh <junho767@naver.com>
2025-02-21 09:41:00 +0900

    feat : 주문 추가 기능 구현
    1. 주문 추가 로직 구현

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/entity/OrderInfo.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/entity/OrderInfo.java
@@ -1,13 +1,13 @@
 package com.java.NBE4_5_1_8.domain.orderinfo.entity;
 
-import com.java.NBE4_5_1_8.domain.orderitem.OrderItem;
+import com.java.NBE4_5_1_8.domain.orderitem.entity.OrderItem;
 import com.java.NBE4_5_1_8.global.entity.BaseTime;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 @Entity
 @Getter
@@ -21,6 +21,13 @@ public class OrderInfo extends BaseTime {
     private String memberEmail;
     private String memberAddress;
 
-    @OneToMany(mappedBy = "orderInfo")
-    private List<OrderItem> orderItems;
+    @OneToMany(mappedBy = "orderInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public static OrderInfo createOrderInfo(String memberEmail) {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.setOrderStatus("장바구니");
+        orderInfo.setMemberEmail(memberEmail);
+        return orderInfo;
+    }
 }
