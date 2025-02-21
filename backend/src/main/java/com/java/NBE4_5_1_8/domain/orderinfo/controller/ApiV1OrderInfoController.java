14a4e9a3b8503bca55e81c8350af9a5f00bcbae1
wnsgh <junho767@naver.com>
2025-02-21 09:39:53 +0900

    feat : 주문 추가 기능 구현
    1. Post /orders/add 구현

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/controller/ApiV1OrderInfoController.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/orderinfo/controller/ApiV1OrderInfoController.java
@@ -1,12 +1,23 @@
 package com.java.NBE4_5_1_8.domain.orderinfo.controller;
 
+import com.java.NBE4_5_1_8.domain.orderinfo.dto.OrderDto;
+import com.java.NBE4_5_1_8.domain.orderinfo.service.OrderInfoService;
+import com.java.NBE4_5_1_8.global.response.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class ApiV1OrderInfoController {
+    private final OrderInfoService orderInfoService;
 
+    @PostMapping("/add")
+    public RsData<Long> create(@RequestBody OrderDto orderDto) {
+        Long orderItemId = orderInfoService.addOrderInfo(orderDto);
+        return RsData.success(orderItemId, "장바구니에 성공적으로 담았습니다.");
+    }
 }
