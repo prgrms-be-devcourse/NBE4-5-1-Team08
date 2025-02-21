2bc8430ba464ae5a5bf34962629af68ddc4aafff
jeonghyeon-ohk <ok6737@naver.com>
2025-02-21 17:30:42 +0900

    feat: 상품 생성 시 유효성 검사를 위해 @Valid 어노테이션 추가

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/item/controller/ApiV1ItemController.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/item/controller/ApiV1ItemController.java
@@ -7,6 +7,8 @@ import com.java.NBE4_5_1_8.domain.item.service.ItemService;
 import com.java.NBE4_5_1_8.global.response.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +21,7 @@ public class ApiV1ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public RsData<ItemDto> createItem(@RequestBody ItemForm requestForm) {
+    public RsData<ItemDto> createItem(@RequestBody @Valid ItemForm requestForm) {
         Item item = itemService.createItem(requestForm);
         return RsData.success(new ItemDto(item), "상품 등록 성공");
     }
@@ -28,8 +30,7 @@ public class ApiV1ItemController {
     public RsData<List<ItemDto>> getItemList() {
         List<Item> items = itemService.getItemList();
         return RsData.success(
-                items
-                        .stream()
+                items.stream()
                         .map(ItemDto::new)
                         .toList(),
                 "상품 조회 성공");
