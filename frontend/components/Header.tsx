fc1f90088344f38759a59a6f487b8c2fefe1d98c
theGreatKang98 <77500386+theGreatKang98@users.noreply.github.com>
2025-02-21 16:16:45 +0900

    feat: fix header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import {useEffect, useState} from "react";
 import {usePathname} from "next/navigation";
 
 const Header = () => {
-    const [isLogin, setIsLogin] = useState(false);
     const pathname = usePathname();
-
-    useEffect(() => {
-        const userToken = localStorage.getItem("userToken");
-        setIsLogin(!!userToken);
-    }, []);
 
     const handleLogout = () => {
         localStorage.removeItem("userToken");
-        setIsLogin(false);
     };
 
     return (
@@ -23,16 +15,9 @@ const Header = () => {
                 <h1 className="text-xl font-bold">Grids & Circles</h1>
                 {pathname.startsWith("/admin") && (
                     <div>
-                        {isLogin ? (
-                            <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
-                                로그아웃
-                            </button>
-                        ) : (
-                            <button onClick={() => (window.location.href = "/login")}
-                                    className="bg-blue-500 px-4 py-2 rounded">
-                                로그인
-                            </button>
-                        )}
+                        <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
+                            로그아웃
+                        </button>
                     </div>
                 )}
             </div>
