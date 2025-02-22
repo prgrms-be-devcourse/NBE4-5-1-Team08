045e4563d4764f01381165ec25c2a096a6749200
jeonghyeon-ohk <ok6737@naver.com>
2025-02-22 18:36:00 +0900

    refactor: RsData 클래스에 Http 코드 필드 추가 및 성공/실패 메서드 수정

--- backend/src/main/java/com/java/NBE4_5_1_8/global/response/RsData.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/response/RsData.java
@@ -1,32 +1,35 @@
 package com.java.NBE4_5_1_8.global.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 @AllArgsConstructor
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RsData<T> {
+    private String code;
     private boolean success;
     private String message;
     private T data;
 
-    public static <T> ResponseEntity<RsData<T>> success(HttpStatus status, String message) {
-        return success(status, null, message);
+    public static <T> RsData<T> success(String code, String message) {
+        return new RsData<>(code, true, message, null);
     }
 
-    public static <T> ResponseEntity<RsData<T>> success(HttpStatus status, T data, String message) {
-        return ResponseEntity
-                .status(status)
-                .body(new RsData<>(true, message, data));
+    public static <T> RsData<T> success(String code, T data, String message) {
+        return new RsData<>(code, true, message, data);
     }
 
-    public static <T> ResponseEntity<RsData<T>> failure(HttpStatus status, String message) {
-        return ResponseEntity
-                .status(status)
-                .body(new RsData<>(false, message, null));
+    public static <T> RsData<T> failure(String code, String message) {
+        return new RsData<>(code, false, message, null);
     }
+
+    @JsonIgnore
+    public int getStatusCode() {
+        return Integer.parseInt(code);
+    }
+
 }
