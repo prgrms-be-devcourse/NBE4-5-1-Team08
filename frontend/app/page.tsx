1a8da3dbdc850bdb88a3e016213494828d999370
theGreatKang98 <77500386+theGreatKang98@users.noreply.github.com>
2025-02-24 14:50:35 +0900

    refactor: refactor use only usable items
    재고 없는 상품 filter

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,16 +2,22 @@ import {client} from "@/app/api/client";
 import ClientHomePage from "@/app/ClientHomePage";
 
 const HomePage = async () => {
-    const rsData = await client.GET('/v1/items');
+    let rsData;
 
-    return (
-        <>
-            {rsData?.data?.success ? (
-                <ClientHomePage itemList={rsData.data.data!}/>
-            ) : (
-                <div>failed</div>
-            )}
-        </>
-    );
-}
+    try {
+        rsData = await client.GET('/v1/items');
+
+        if (!rsData?.data?.success || !rsData.data.data) {
+            return <div>데이터 로드 실패</div>
+        }
+
+        const filteredItems = rsData.data.data.filter(item => item.stockQuantity > 0);
+
+        return <ClientHomePage itemList={filteredItems}/>;
+    } catch (error) {
+        console.error("상품 데이터를 불러오는 중 오류 발생:", error);
+        return <div>상품 데이터를 불러오는 데 실패했습니다. 다시 시도해주세요.</div>;
+    }
+};
+
 export default HomePage;
