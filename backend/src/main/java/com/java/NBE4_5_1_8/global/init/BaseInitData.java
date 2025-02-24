2821fe0306eaf40c376aac541b2e33d121d31883
서세훈 <ssa0100@naver.com>
2025-02-25 05:29:26 +0900

    feat: use Category enum for item type
    "커피콩" -> Category.COFFEE_BEAN

--- backend/src/main/java/com/java/NBE4_5_1_8/global/init/BaseInitData.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/init/BaseInitData.java
@@ -1,6 +1,7 @@
 package com.java.NBE4_5_1_8.global.init;
 
 import com.java.NBE4_5_1_8.domain.item.dto.ItemForm;
+import com.java.NBE4_5_1_8.domain.item.entity.Category;
 import com.java.NBE4_5_1_8.domain.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,9 +35,9 @@ public class BaseInitData {
         }
 
         // 샘플 아이템 데이터 생성
-        itemService.createItem(new ItemForm("Columbia Nariñó", "커피콩", "고소한 풍미의 콜롬비아 원두", 200, 5000, null));
-        itemService.createItem(new ItemForm("Brazil Serra Do Caparaó", "커피콩", "밸런스 좋은 브라질 원두", 200, 5000, null));
-        itemService.createItem(new ItemForm("Columbia Nariñó", "커피콩", "산미가 조화로운 콜롬비아 원두", 200, 5000, null));
+        itemService.createItem(new ItemForm("Columbia Nariñó", Category.COFFEE_BEAN, "고소한 풍미의 콜롬비아 원두", 200, 5000, null));
+        itemService.createItem(new ItemForm("Brazil Serra Do Caparaó", Category.COFFEE_BEAN, "밸런스 좋은 브라질 원두", 200, 5000, null));
+        itemService.createItem(new ItemForm("Columbia Nariñó", Category.COFFEE_BEAN, "산미가 조화로운 콜롬비아 원두", 200, 5000, null));
 
     }
 }
