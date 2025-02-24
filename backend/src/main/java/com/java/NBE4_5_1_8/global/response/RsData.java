f98849b2372a74cf0b767f3b87c541a371303e30
jeonghyeon-ohk <ok6737@naver.com>
2025-02-24 10:31:15 +0900

    refactor: RsData에서 message의 타입을 String에서 MessageType으로 변경

--- backend/src/main/java/com/java/NBE4_5_1_8/global/response/RsData.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/response/RsData.java
@@ -2,6 +2,7 @@ package com.java.NBE4_5_1_8.global.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.java.NBE4_5_1_8.global.message.MessageType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -12,18 +13,18 @@ import org.springframework.http.HttpStatus;
 public class RsData<T> {
     private HttpStatus status;
     private boolean success;
-    private String message;
+    private MessageType message;
     private T data;
 
-    public static <T> RsData<T> success(HttpStatus status, String message) {
+    public static <T> RsData<T> success(HttpStatus status, MessageType message) {
         return new RsData<>(status, true, message, null);
     }
 
-    public static <T> RsData<T> success(HttpStatus status, T data, String message) {
+    public static <T> RsData<T> success(HttpStatus status, T data, MessageType message) {
         return new RsData<>(status, true, message, data);
     }
 
-    public static <T> RsData<T> failure(HttpStatus status, String message) {
+    public static <T> RsData<T> failure(HttpStatus status, MessageType message) {
         return new RsData<>(status, false, message, null);
     }
 
