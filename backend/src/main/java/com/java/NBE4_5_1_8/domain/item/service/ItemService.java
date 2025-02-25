88b12095ed43afb616e6d99102dfe72f174db434
jeonghyeon-ohk <ok6737@naver.com>
2025-02-25 09:52:15 +0900

    fix: backend 충돌 제거
    # Conflicts:
    #	backend/src/main/java/com/java/NBE4_5_1_8/domain/item/service/ItemService.java

--- backend/src/main/java/com/java/NBE4_5_1_8/domain/item/service/ItemService.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/domain/item/service/ItemService.java
@@ -112,8 +112,4 @@ public class ItemService {
     public void deleteItem(Item item) {
         itemRepository.delete(item);
     }
-
-    public List<Item> findAll() {
-        return itemRepository.findAll();
-    }
 }
