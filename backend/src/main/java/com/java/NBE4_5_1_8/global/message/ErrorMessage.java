d9b248c6396cef7c91cb19fbc2bc90ff8f895141
서세훈 <ssa0100@naver.com>
2025-02-25 11:09:23 +0900

    feat: add error and success messages

--- backend/src/main/java/com/java/NBE4_5_1_8/global/message/ErrorMessage.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/message/ErrorMessage.java
@@ -10,7 +10,10 @@ public enum ErrorMessage implements MessageType{
     ORDER_NOT_FOUND("존재하지 않는 주문입니다."),
     INVALID_REQUEST("잘못된 요청입니다."),
     ITEM_CANNOT_BE_DELETED("주문된 상품은 삭제할 수 없습니다."),
-    ORDER_CANNOT_BE_DELETED("삭제 불가능한 주문입니다.");
+    ORDER_CANNOT_BE_DELETED("삭제 불가능한 주문입니다."),
+    CATEGORY_NOT_FOUND("존재하지 않는 카테고리입니다."),
+    CATEGORY_ALREADY_EXISTS("이미 존재하는 카테고리입니다.");
+
 
     private final String message;
 }
