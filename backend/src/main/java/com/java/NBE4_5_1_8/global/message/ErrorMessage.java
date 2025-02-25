347282d68048ffbfc05344355da8535cd19b2d24
wnsgh <junho767@naver.com>
2025-02-25 23:02:09 +0900

    feat: 재고 관리 예외 처리 추가

--- backend/src/main/java/com/java/NBE4_5_1_8/global/message/ErrorMessage.java
+++ backend/src/main/java/com/java/NBE4_5_1_8/global/message/ErrorMessage.java
@@ -13,7 +13,9 @@ public enum ErrorMessage implements MessageType{
     ORDER_CANNOT_BE_DELETED("삭제 불가능한 주문입니다."),
     CATEGORY_NOT_FOUND("존재하지 않는 카테고리입니다."),
     CATEGORY_ALREADY_EXISTS("이미 존재하는 카테고리입니다."),
-    ITEM_BE_DISABLED("주문 내역이 있는 상품은 비활성화 처리되었습니다.");
+    ITEM_BE_DISABLED("주문 내역이 있는 상품은 비활성화 처리되었습니다."),
+    OUT_OF_STUCK("물품의 재고가 부족합니다.");
+
 
 
     private final String message;
